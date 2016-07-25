48d060ecb9d85528027ff219f04f5ae491c63dd9
Maryia Neiman <Maryia_Neiman@epam.com>
2016-07-25 17:58:08 +0300

    second

---
+++ CoreTAF/CoreTAF/Elements/RadioButton .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreTAF.Elements
+{
+    public class RadioButton : Element
+    {
+        void click() { }
+
+        bool isSelect() { return true; }
+
+    }
+
+}
